0ec922d7d0f5f376a355f976f7be1845654e8e2f
himchan94 <himchan9494@gmail.com>
2022-01-02 18:24:36 +0900

    feat: Imported ImageLoader to main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+import SpriteSheet from "./SpriteSheet.js";
+import { loadImage, loadLevel } from "./loaders.js";
+
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+
+function drawBackground(background, context, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let x = x1; x < x2; ++x) {
+      for (let y = y1; y < y2; ++y) {
+        sprites.drawTile(background.tile, context, x, y);
+      }
+    }
+  });
+}
+
+loadImage("/img/tiles.png").then((image) => {
+  const sprites = new SpriteSheet(image);
+  sprites.define("ground", 0, 0);
+  sprites.define("sky", 3, 23);
+
+  console.log("스프라이트", sprites);
+
+  loadLevel("1-1").then((level) => {
+    level.backgrounds.forEach((bg) => {
+      drawBackground(bg, context, sprites);
+    });
+  });
+});
